4072b44e2a54f642476ad8b04971ce7e23059de9
github username <github email address>
2019-08-06 14:16:25 -0400

    add routes to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
+  #loads the signup form
   def new
    @user = User.new
   end
+
+  #user signup
   def create
+    @user = User.new(user_params)
+    if @user.save
+      #logging in user
+      session[:user_id] = @user.id
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
 
   end
+
 end
